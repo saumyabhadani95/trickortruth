338c18ba146962b4fb0709acb5a2234a4cbeb06f
Saumya Bhadani <saumya@pop-os.localdomain>
2023-05-07 16:05:11 -0400

    storing files as json

--- backend/src/read_json.py
+++ backend/src/read_json.py
@@ -1,6 +1,8 @@
 import json
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, request, url_for, redirect, flash, make_response, jsonify
+import os
+import glob
 
 app = Flask(__name__)
 
@@ -17,6 +19,24 @@ def parse_html():
 	url = soup.find("meta", {"property": "og:url"}).get("content")
 	typee = soup.find("meta", {"property": "og:type"}).get("content")
 	return jsonify({"title":title,"description":description,"url":url,"type":typee})
+
+@app.route('/storedata', methods=['GET','POST'])
+def store_data():
+	cwd = os.getcwd()
+	urll = request.args.get('url')
+	misleading_rating = request.args.get('misleading_rating')
+	biased_rating = request.args.get('biased_rating')
+	clarity_rating = request.args.get('clarity_rating')
+	os.chdir("../data/collection/")
+	all_files = sorted(glob.glob("*.json.gz"))
+	new_file_number = 1
+	if all_files:
+		latest_file = max(all_files, key=lambda fn: int(fn.split(".")[0]))
+		new_file_number = int(latest_file.split(".")[0]) + 1
+	with open(str(new_file_number)+".json","w") as fout:
+		fout.write(json.dumps({"url":url,"misleading_rating":misleading_rating,"biased_rating":biased_rating,"clarity_rating":clarity_rating}))
+	os.chdir(cwd)
+	return "Done!"
 
 @app.route('/getjson', methods=['GET'])
 def get_json():
@@ -35,4 +55,4 @@ def add_headers(response):
     return response
   
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0",port=6000)
